3ae3c58d94570173986c6953e185cbcf3e730e67
markuspg <markuspg@users.noreply.github.com>
2021-04-06 19:21:51 +0200

    Import MessageDialog class

---
+++ src/messagedialog.cpp
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2006-2014 Derrick Moser <derrick_moser@yahoo.com>
+ * Copyright (C) 2021      Markus Prasser <markuspg@gmx.de>
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the licence, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  You may also obtain a copy of the GNU General Public License
+ * from the Free Software Foundation by visiting their web site
+ * (http://www.fsf.org/) or by writing to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "messagedialog.h"
+#include "diffuse_globals.h"
+
+namespace Df = Diffuse;
+
+Df::MessageDialog::MessageDialog(Gtk::MessageType type, const Glib::ustring &s)
+    : Gtk::MessageDialog{s, false, type,
+                         Gtk::MESSAGE_ERROR == type ? Gtk::BUTTONS_OK
+                                                    : Gtk::BUTTONS_OK_CANCEL,
+                         true} {
+  set_title(APP_NAME);
+}
+
+Df::MessageDialog::MessageDialog(Gtk::Window &parent, Gtk::MessageType type,
+                                 const Glib::ustring &s)
+    : Gtk::MessageDialog{parent,
+                         s,
+                         false,
+                         type,
+                         Gtk::MESSAGE_ERROR == type ? Gtk::BUTTONS_OK
+                                                    : Gtk::BUTTONS_OK_CANCEL,
+                         true} {
+  gtk_window_set_destroy_with_parent(GTK_WINDOW(gobj()), true);
+  set_title(APP_NAME);
+}
