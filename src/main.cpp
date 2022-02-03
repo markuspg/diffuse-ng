b4e95a5c911185f9a5ff8f3995aa9df7c7438d96
markuspg <markuspg@users.noreply.github.com>
2022-02-03 18:44:13 +0100

    Add C++ style license header

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,26 @@
+/* Diffuse - A graphical tool for merging and comparing text files
+ *
+ * Copyright (C) 2006-2014 Derrick Moser <derrick_moser@yahoo.com>
+ * Copyright (C) 2022      Markus Prasser
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
 int main(int argc, char *argv[]) {
 
 /*
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-
-# Copyright (C) 2006-2014 Derrick Moser <derrick_moser@yahoo.com>
-#
-# This program is free software; you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free Software
-# Foundation; either version 2 of the license, or (at your option) any later
-# version.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-# details.
-#
-# You should have received a copy of the GNU General Public License along with
-# this program.  You may also obtain a copy of the GNU General Public License
-# from the Free Software Foundation by visiting their web site
-# (http://www.fsf.org/) or by writing to the Free Software Foundation, Inc.,
-# 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
 import codecs
 import gettext
 import locale
