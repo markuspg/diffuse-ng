882a2e8ea9dae2639ebacba77bc365d1719d4cf7
markuspg <markuspg@users.noreply.github.com>
2022-03-02 22:34:27 +0100

    Begin importing Resources

---
+++ src/df_resources.h
@@ -0,0 +1,45 @@
+/* Diffuse - A graphical tool for merging and comparing text files
+ *
+ * Copyright (C) 2006-2014 Derrick Moser <derrick_moser@yahoo.com>
+ * Copyright (C) 2022      Markus Prasser
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the license, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  You may also obtain a copy of the GNU General Public License
+ * from the Free Software Foundation by visiting their web site
+ * (http://www.fsf.org/) or by writing to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef DF_RESOURCES_H
+#define DF_RESOURCES_H
+
+#include <glibmm/ustring.h>
+
+#include <map>
+
+namespace Diffuse {
+class Resources {
+public:
+  Resources();
+
+private:
+  void setKeyBinding(const Glib::ustring &ctx, const Glib::ustring &s,
+                     const Glib::ustring &v);
+
+  // Default keybindings
+  std::map<void *, void *> keybindings;
+  std::map<void *, void *> keybindings_lookup;
+};
+} // namespace Diffuse
+
+#endif // DF_RESOURCES_H
